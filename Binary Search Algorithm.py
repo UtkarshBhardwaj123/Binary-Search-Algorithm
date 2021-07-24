9c7ea0df1eb3462e8a5e729c778d782523a7accb
Utkarsh Bhardwaj <61777396+UtkarshBhardwaj123@users.noreply.github.com>
2021-07-25 00:38:36 +0530

    Update Binary Search Algorithm.py

--- Binary Search Algorithm.py
+++ Binary Search Algorithm.py
@@ -1,6 +1,6 @@
 Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+>>>#returns index of x if present in arr, else -1
    def binarysearch(arr,l,r,x):
           #check base case
           if(r>=l):
